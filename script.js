438ad53cdc8eac9be6107b93eba9f28c0d51f2f7
Aylina Ospina Cruz <e227422@edu.sbl.ch>
2023-10-30 12:15:02 +0100

    Aufgabe 1 und 2

--- script.js
+++ script.js
@@ -4,7 +4,38 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
-  }
+
+if(currentElement === 'e') {
+  result.push("")
+}else if ( currentElement === 'E') {
+    result.push("") 
+  
+}else {
+  result.push(currentElement)
+}
+}
+
   return result.join("")
 }
+
+
+
+export function aufgabe02(args) {
+  const input = args
+  const result = []
+
+for (let i = 0; i < input.length; i++) {
+const currentElement = input[i]
+
+const capitalizedElement = currentElement.toUpperCase()
+
+result.push(capitalizedElement)
+
+}
+return result.join("")
+}
+
+
+
+
+
