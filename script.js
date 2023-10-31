e3da72d5571e7356ed70ab8374f7fa03f2b1add8
Aylina Ospina Cruz <e227422@edu.sbl.ch>
2023-10-31 08:36:08 +0100

    Aufgabe 3

--- script.js
+++ script.js
@@ -26,14 +26,39 @@ export function aufgabe02(args) {
 
 for (let i = 0; i < input.length; i++) {
 const currentElement = input[i]
-
+//Die Buchstaben werden der Reihen nach überprüft.
 const capitalizedElement = currentElement.toUpperCase()
 
 result.push(capitalizedElement)
-
+//die Ergebnisse werden zusammengefügt.
 }
+//Das Ergebniss wird zurückgegeben.
 return result.join("")
 }
+
+
+export function aufgabe03(args) {
+  const input = args
+  const result = []
+let count = 0
+//prüft ob ein e enhalten ist.
+
+for (let i = 0; i < input.length; i++) {
+const currentElement = input[i]
+
+
+if ( currentElement===`e`)
+count = count + 1 
+
+if(currentElement===`E`)
+count = count + 1 
+
+
+}
+return count
+}
+
+
 
 
 
